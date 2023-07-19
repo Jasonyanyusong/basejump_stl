8732e7a9b8ad9f571de0249b8a6d0ac0bca1fff2
Dan Petrisko <petrisko@cs.washington.edu>
2023-07-19 01:42:24 -0700

    Update bsg_noc_pkg.v

--- bsg_noc/bsg_noc_pkg.v
+++ bsg_noc/bsg_noc_pkg.v
@@ -1,5 +1,3 @@
-`ifndef BSG_NOC_PKG_V
-`define BSG_NOC_PKG_V
 
 // direction type
 package bsg_noc_pkg;
@@ -7,5 +5,3 @@ package bsg_noc_pkg;
   // https://www.veripool.org/issues/1442-Verilator-Enum-values-without-explicit-widths-are-considered-unsized
   typedef enum logic[2:0] {P=3'd0, W, E, N, S} Dirs;
 endpackage
-
-`endif
